b4fdd938a177aee69961da882f9beef6fc8d7f82
Eloisa Fagundes <lola.fernanda@hotmail.com>
2020-07-12 15:13:38 -0300

    adiciona hover e links as midias sociais

--- portfolio/src/components/SocialMedias/SocialMedias.js
+++ portfolio/src/components/SocialMedias/SocialMedias.js
@@ -6,37 +6,70 @@ const SocialWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
 
-  @media screen and (max-device-width: 1200px){
+  @media screen and (max-device-width: 1200px) {
     /* background-color: red; */
-        /* justify-content: space-between;
+    /* justify-content: space-between;
         flex-direction: row;
         position: absolute;
         margin: 4vh;
         height: 5vh;
         width: 82vw;
         z-index: 2; */
-        display: none;
-    }
+    display: none;
+  }
 `;
 
 const SocialIconStyled = styled.img`
-   width: 15vh; 
-   /* @media screen and (max-device-width: 1200px){
-        width: 20vw;
-        background-color: #6580ac;
-        border-radius: 90px;
-    } */
-`
+  width: 15vh;
+  cursor: pointer;
+
+  :hover,
+  :focus {
+  color: #333;
+  background-color: rgba(255,255,255,0.1);
+  border-radius: 90px;
+}
+`;
 
 function SocialMedias() {
   return (
     <SocialWrapper>
-        <SocialIconStyled src={require("../../images/linkedin.png")} alt="bla"/>
-        <SocialIconStyled src={require("../../images/github.png")} alt="bla"/>
-        <SocialIconStyled src={require("../../images/email.png")} alt="bla"/>
-        <SocialIconStyled src={require("../../images/whatsapp.png")} alt="bla"/>
+      <SocialIconStyled
+        src={require("../../images/linkedin.png")}
+        alt="linkedin logo contorno branco vazado"
+        onClick={() =>
+          window.open(
+            "https://www.linkedin.com/in/eloisa-fernanda-nunes-fagundes-1b72bbb1/",
+            "_blank"
+          )
+        }
+      />
+      <SocialIconStyled
+        src={require("../../images/github.png")}
+        alt="github logo branco vazado"
+        onClick={() =>
+          window.open("https://github.com/EloisaFagundes", "_blank")
+        }
+      />
+      <SocialIconStyled
+        src={require("../../images/email.png")}
+        alt="email logo carta branca vazada"
+        onClick={() =>
+          window.open("mailto:lola.fernanda@hotmail.com", "_blank")
+        }
+      />
+      <SocialIconStyled
+        src={require("../../images/whatsapp.png")}
+        alt="whatsapp logo todo branco"
+        onClick={() =>
+          window.open(
+            "https://web.whatsapp.com/send?phone=+5511944531080",
+            "_blank"
+          )
+        }
+      />
     </SocialWrapper>
   );
 }
